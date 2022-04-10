d2b6a9e703c54025abc3ae1d1dec5666a2cdb155
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-10 15:44:57 +0100

    add styles and functionality for mobile navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import "../styles/main.scss";
+const hamburger = document.querySelector("[data-hamburger]");
+const hamburgerFill = document.querySelector("[data-hamburger-fill]");
+const body = document.querySelector("body");
+const mobileNav = document.querySelector("[data-mobile-nav]");
+
+hamburger.addEventListener("click", () => {
+  body.classList.toggle("no__scroll");
+  mobileNav.classList.toggle("hide");
+  hamburgerFill.classList.toggle("dim");
+});
