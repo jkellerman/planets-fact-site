98904ef89b43fec2a7c247e7b82ebadd17f910a1
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-12 22:17:59 +0100

    import json file and populate earth data

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,23 @@
 import "../styles/main.scss";
+import data from "../data.json";
+
 const hamburger = document.querySelector("[data-hamburger]");
 const hamburgerFill = document.querySelector("[data-hamburger-fill]");
 const body = document.querySelector("body");
 const mobileNav = document.querySelector("[data-mobile-nav]");
+
+const getPlanet = (planet) => {
+  document.querySelector("[data-name]").innerText = planet.name;
+  document.querySelector("[data-planet]").src = planet.images.planet;
+  document.querySelector("[data-p]").innerText = planet.overview.content;
+  document.querySelector("[data-wiki]").href = planet.overview.source;
+  document.querySelector("[data-rotation]").innerText = planet.rotation;
+  document.querySelector("[data-revolution]").innerText = planet.revolution;
+  document.querySelector("[data-radius]").innerText = planet.radius;
+  document.querySelector("[data-temp]").innerText = planet.temperature;
+};
+
+getPlanet(data[2]);
 
 hamburger.addEventListener("click", () => {
   body.classList.toggle("no__scroll");
