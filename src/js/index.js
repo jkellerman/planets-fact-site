1f936e169d65307b6fadb4def3db39290ccdd094
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-13 11:57:30 +0100

    add click events for tabs

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,9 @@ const hamburgerFill = document.querySelector("[data-hamburger-fill]");
 const body = document.querySelector("body");
 const mobileNav = document.querySelector("[data-mobile-nav]");
 const desktopLinks = Array.from(document.querySelectorAll(".desktop__links"));
+const overview = document.querySelector("[data-overview]");
+const structure = document.querySelector("[data-structure]");
+const surface = document.querySelector("[data-surface]");
 
 const getPlanet = (planet) => {
   // Split string from assets to use for adding images dynamically
@@ -16,8 +19,8 @@ const getPlanet = (planet) => {
     .pop()
     .replace(/\.svg/g, "");
   // dynamically render data to browser
-  document.querySelector("[data-planet]").src = assets[jsonImagePath];
   const name = (document.querySelector("[data-name]").innerText = planet.name);
+  document.querySelector("[data-planet]").src = assets[jsonImagePath];
   document.querySelector("[data-content]").innerText = planet.overview.content;
   document.querySelector("[data-wiki]").href = planet.overview.source;
   document.querySelector("[data-rotation]").innerText = planet.rotation;
@@ -25,8 +28,9 @@ const getPlanet = (planet) => {
   document.querySelector("[data-radius]").innerText = planet.radius;
   document.querySelector("[data-temp]").innerText = planet.temperature;
 
-  // Highlight tab with background color based on planet
-  document.querySelector("[data-overview]").style.backgroundColor = "red";
+  // Highlight overview tab on pageload
+  overview.style.backgroundColor = `var(--${name.toLowerCase()})`;
+  overview.style.border = "none";
 };
 
 // render earth to browser on load
@@ -47,5 +51,40 @@ desktopLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     const index = desktopLinks.indexOf(link);
     getPlanet(data[index]);
+    removeTabStyles(structure, surface);
   });
 });
+
+// click event for internal structure tab
+overview.addEventListener("click", () => {
+  // get the current innertext of h1 tag to use for color change
+  const name = document.querySelector("[data-name]").innerText.toLowerCase();
+  addTabStyles(overview, name);
+  removeTabStyles(structure, surface);
+});
+structure.addEventListener("click", () => {
+  // get the current innertext of h1 tag to use for color change
+  const name = document.querySelector("[data-name]").innerText.toLowerCase();
+  addTabStyles(structure, name);
+  removeTabStyles(overview, surface);
+});
+
+surface.addEventListener("click", () => {
+  // get the current innertext of h1 tag to use for color change
+  const name = document.querySelector("[data-name]").innerText.toLowerCase();
+  addTabStyles(surface, name);
+  removeTabStyles(overview, structure);
+});
+
+function addTabStyles(tab, color) {
+  tab.style.backgroundColor = `var(--${color.toLowerCase()})`;
+  tab.style.border = "none";
+  tab.style.transition = "0.25s ease-in-out";
+}
+
+function removeTabStyles(tab, tab2) {
+  tab.style.backgroundColor = null;
+  tab.style.border = null;
+  tab2.style.backgroundColor = null;
+  tab2.style.border = null;
+}
