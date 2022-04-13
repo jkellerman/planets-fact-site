e087d3a3711a2799a044bf1b6ed20be463091097
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-04-13 09:10:06 +0100

    import assets to add images dynamically

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,24 @@
 import "../styles/main.scss";
 import data from "../data.json";
+import assets from "../assets/*.svg";
+import assetsPng from "../assets/*.png";
 
 const hamburger = document.querySelector("[data-hamburger]");
 const hamburgerFill = document.querySelector("[data-hamburger-fill]");
 const body = document.querySelector("body");
 const mobileNav = document.querySelector("[data-mobile-nav]");
+const navLinks = document.querySelectorAll("li");
+console.log(navLinks);
 
 const getPlanet = (planet) => {
+  // Split string from assets to use for adding images dynamically
+  const jsonImagePath = planet.images.planet
+    .split("/")
+    .pop()
+    .replace(/\.svg/g, "");
+  document.querySelector("[data-planet]").src = assets[jsonImagePath];
   document.querySelector("[data-name]").innerText = planet.name;
-  document.querySelector("[data-planet]").src = planet.images.planet;
-  document.querySelector("[data-p]").innerText = planet.overview.content;
+  document.querySelector("[data-content]").innerText = planet.overview.content;
   document.querySelector("[data-wiki]").href = planet.overview.source;
   document.querySelector("[data-rotation]").innerText = planet.rotation;
   document.querySelector("[data-revolution]").innerText = planet.revolution;
@@ -17,7 +26,7 @@ const getPlanet = (planet) => {
   document.querySelector("[data-temp]").innerText = planet.temperature;
 };
 
-getPlanet(data[2]);
+getPlanet(data[3]);
 
 hamburger.addEventListener("click", () => {
   body.classList.toggle("no__scroll");
